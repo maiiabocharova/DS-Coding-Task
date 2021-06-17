bd01d45c6e19ccdca6557e4c9f54b20b14572a99
maiia.bocharova <maiia.bocharova@powow.ai>
2021-06-17 08:31:27 +0300

    part1 of a coding task

--- part1.py
+++ part1.py
@@ -1,16 +1,28 @@
 import requests
 from lxml import html
 import time
-
+from bs4 import BeautifulSoup
 hpilosophy_link = "https://en.wikipedia.org/wiki/Philosophy"
 visited_urls = set()
-start_url = input()
+url = input()
 
-while url != hpilosophy_link or url not in visited_urls:
-    visited_urls.add(url)
-    page = requests.get(url=url).text
-    root = html.fromstring(page)
-    href = root.xpath("//div/p/a[not(@class)]/@href")[0]
+while url != hpilosophy_link and url not in visited_urls:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    print(r.url)  # Print current url
+
+    # getting rid of unnecessary stuff
+    content = soup.find(id='mw-content-text')
+    for t in content.find_all(class_=['navbox', 'vertical-navbox', 'toc']):
+        t.replace_with("")
+
+    for s in content.find_all(
+            ['span', 'small', 'sup,', 'i', 'table']):  # remove spans and smalls with language, pronounciation
+        s.replace_with("")
+
+    root = html.fromstring(str(content))
+    href = root.xpath("//div/p/a/@href")[0]
+
     url = "https://en.wikipedia.org" + href
-    print(url)
+    print(href)
     time.sleep(0.5)
